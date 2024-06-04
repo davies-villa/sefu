a93f222748570cdef80d6a49a47b5c11f10f29a9
Davies Gotosa <daviesgotosa10@gmail.com>
2024-06-04 11:19:38 +0200

    yow

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,25 @@
-document.addEventListener("click", function(e) {
+document.addEventListener("click", function (e) {
     closeAllSelect(e.target);
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const loader = document.getElementById('loader');
+    const loaderImage = document.getElementById('loaderImage');
+    const images = ['img/loader1.png', 'img/loader2.png'];
+    let currentIndex = 0;
+    const intervalTime = 500;
+    const duration = 10000;
 
+    const interval = setInterval(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        loaderImage.src = images[currentIndex];
+    }, intervalTime);
+
+    setTimeout(() => {
+        clearInterval(interval);
+        loader.classList.add('hidden');
+    }, duration);
+});
 
 function closeAllSelect(elmnt) {
     var selects = document.getElementsByClassName("select-items");
@@ -15,17 +32,17 @@ function closeAllSelect(elmnt) {
 }
 
 var customSelects = document.querySelectorAll(".select-wrapper");
-customSelects.forEach(function(select) {
+customSelects.forEach(function (select) {
     var selectSelected = select.querySelector(".select-selected");
     var selectItems = select.querySelector(".select-items");
 
-    selectSelected.addEventListener("click", function(e) {
+    selectSelected.addEventListener("click", function (e) {
         e.stopPropagation();
         closeAllSelect(this);
         selectItems.classList.toggle("select-show");
     });
 
-    selectItems.addEventListener("click", function(e) {
+    selectItems.addEventListener("click", function (e) {
         var target = e.target;
         if (!target.matches(".select-items div")) return;
         selectSelected.textContent = target.textContent;

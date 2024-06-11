a7f63bf9657ee51542d5f361d0f18e2d5e14a104
Davies Gotosa <daviesgotosa10@gmail.com>
2024-06-11 21:50:26 +0200

    Added a new slider feature

--- js/custom.js
+++ js/custom.js
@@ -2,24 +2,6 @@ document.addEventListener("click", function (e) {
     closeAllSelect(e.target);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loader = document.getElementById('loader');
-    const loaderImage = document.getElementById('loaderImage');
-    const images = ['img/loader1.png', 'img/loader2.png'];
-    let currentIndex = 0;
-    const intervalTime = 500;
-    const duration = 10000;
-
-    const interval = setInterval(() => {
-        currentIndex = (currentIndex + 1) % images.length;
-        loaderImage.src = images[currentIndex];
-    }, intervalTime);
-
-    setTimeout(() => {
-        clearInterval(interval);
-        loader.classList.add('hidden');
-    }, duration);
-});
 
 function closeAllSelect(elmnt) {
     var selects = document.getElementsByClassName("select-items");
